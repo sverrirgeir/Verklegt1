753ae9b2c3047444f128928de9aa44a1cfc771d0
kristofere17 <kristofer_ey@hotmail.com>
2018-12-04 16:30:12 +0000

    main uppfært

--- Main.py
+++ Main.py
@@ -1,8 +1,11 @@
 from functions.print_main_menu import print_main_menu
-
+from functions.determine_choice import determine_choice
 def main():
-    while True:
+    decision = True
+    while decision == True:
         choice = print_main_menu()
+        decision = determine_choice(choice)
+
         
 
 main()
