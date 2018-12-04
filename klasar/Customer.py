220fcb4b42e1bf5384e31f2ca0bf80371c7482d4
kristofere17 <kristofer_ey@hotmail.com>
2018-12-04 14:34:43 +0000

    uppfærður klasi

--- klasar/Customer.py
+++ klasar/Customer.py
@@ -1,2 +1,8 @@
 class Customer(object):
+    def __init__(self, first_name, last_name, Passport_num, credit_card_number):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.full_name = first_name + last_name
+        self.Passport_num = Passport_num
+        self.credit_card_number = credit_card_number
     
