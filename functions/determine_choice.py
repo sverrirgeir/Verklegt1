a6f312f997d5ab17d3ad8fb64ee24a3dfd0f1feb
kristofere17 <kristofer_ey@hotmail.com>
2018-12-04 16:39:13 +0000

    fall sem ver yfir val

---
+++ functions/determine_choice.py
@@ -0,0 +1,13 @@
+def determine_choice(choice):
+    if choice == "1":
+        pass
+    elif choice == "2":
+        pass
+    elif choice == "3":
+        pass
+    elif choice == "4":
+        pass
+    elif choice == "5":
+        pass
+    else:
+        return False
