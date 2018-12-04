fc91474bcd68c3c9355fb2745ec18d058f5b274d
sverrirgeir <sverrirgeir@gmail.com>
2018-12-04 17:21:15 +0000

    uppfært kallað í fall

--- functions/determine_choice.py
+++ functions/determine_choice.py
@@ -1,6 +1,8 @@
+from functions.print_order_menu import print_order_menu
+
 def determine_choice(choice):
     if choice == "1":
-        pass
+        print_order_menu()
     elif choice == "2":
         pass
     elif choice == "3":
