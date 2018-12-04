a969c6c8c1eacfdf7e48542509d66ba0b6fa6d00
kristofere17 <kristofer_ey@hotmail.com>
2018-12-04 16:39:49 +0000

    updated

--- functions/print_main_menu.py
+++ functions/print_main_menu.py
@@ -6,7 +6,9 @@ def print_main_menu():
     choice4 = "4. Verðlisti"
     choice5 = "5. Panta bíl"
     choice6 = "6. Hætta"
-    print("{:^10}".format("Bílaleiga ehf"))
     print("")
-    print("{:^10}\n{:^10}\n{:^10}\n{:^10}\n{:^10}\n{:^10}".format(choice1, choice2, choice3, choice4, choice5, choice6))
-    return input("Valmöguleiki: ")
+    print("\t{:^10}".format("Bílaleiga ehf"))
+    print("")
+    print("\t{:<30}\n\t{:<10}\n\t{:<10}\n\t{:<10}\n\t{:<10}\n\t{:<10}".format(choice1, choice2, choice3, choice4, choice5, choice6))
+    print("")
+    return input("\tValmöguleiki: ")
