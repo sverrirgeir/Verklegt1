e6d8f0b55f9ffeb46a8e6b052f0c2daa11f9475e
zakkimoto <zakkitv@gmail.com>
2018-12-04 14:25:02 +0000

    code

--- klasar/bilafloti.py
+++ klasar/bilafloti.py
@@ -1,1 +1,10 @@
-Class Cars(object):
+Class Cars (object):
+
+    def __init__(self, skra):
+        self.skra = skra
+
+    def allcars(skra):
+        file = open(skra, "r")
+        for text in file:
+            
+    
